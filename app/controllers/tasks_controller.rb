a1a585fcec17eaa3545ae7422ef22b603a46441d
shin-m <fen.mex.cyc@gmail.com>
2016-10-14 06:28:21 +0900

    [Add]タスクのCRUD機能の追加
    ますは一通りの機能を実装するため、エラーチェックやログインのチェックは
    実装していない。

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,12 +9,29 @@ class TasksController < ApplicationController
     end
   end
 
+  def destroy
+    @task = current_user.created_tasks.find(params[:id])
+    @task.destroy!
+    redirect_to root_path, notice: 'タスクを削除しました'
+  end
+
   def index
     user = current_user || User.new
 
     @all_tasks_in_progress = Task.in_progress
     @untouched_tasks       = user.created_tasks.untouched
     @task                  = user.created_tasks.new
+  end
+
+  def update
+    @task = current_user.created_tasks.find(params[:id])
+    if @task.update(task_params)
+      flash[:notice] = 'タスクを更新しました'
+      head :ok
+    else
+      render json: { messages: task.errors.full_messages }, 
+        status: :unprocessable_entity
+    end
   end
 
   private
