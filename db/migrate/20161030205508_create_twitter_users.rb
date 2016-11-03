ffe12a76ccef4aa819ae9b407aa8a78880853952
shin-m <fen.mex.cyc@gmail.com>
2016-11-03 18:52:08 +0900

    [Fix]typoで外部キーが作成されていなかったのを修正

--- db/migrate/20161030205508_create_twitter_users.rb
+++ db/migrate/20161030205508_create_twitter_users.rb
@@ -2,7 +2,7 @@ class CreateTwitterUsers < ActiveRecord::Migration
   def change
     create_table :twitter_users do |t|
       t.string :uid, null: false, index: true
-      t.references :user, forreign_key: true, unique: true, null: false
+      t.references :user, foreign_key: true, unique: true, null: false
       t.timestamps
     end
   end
