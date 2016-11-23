40288b68141b6950779911ece099f34d542be1a8
shin-m <fen.mex.cyc@gmail.com>
2016-11-23 16:07:26 +0900

    [Modify]Twitterのユーザ名とユーザIDを表示するように変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,13 @@
 module ApplicationHelper
-  def url_for_twitter(user)
-    "https://twitter.com/#{user.username}"
+  def url_for_twitter(twitter_user)
+    "https://twitter.com/#{twitter_user.nickname}"
+  end
+
+  def link_twitter_user_name(twitter_user)
+    link_to(url_for_twitter(twitter_user), class: 'h5') do
+      concat content_tag(:span, twitter_user.user.username, class: 'bold')
+      concat content_tag(:span, " @#{twitter_user.nickname}",
+                         class: 'h6 text-muted')
+    end
   end
 end
