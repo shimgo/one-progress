83258bb6abe6123ac5d33c6872bf5ec30ba3abda
shin-m <fen.mex.cyc@gmail.com>
2017-02-26 06:40:37 +0900

    [Modify]認証失敗時に400エラーページに遷移するように変更
    元々のコードはwrite_failure_logが呼び出されないバグがあったため変更。
    (加えて例外発生時にもログは出力されるためログが2重に出ていた)
    
    また、バグでない限りはクライアント側(cookies)の問題であるため、400番台の
    エラーページを使用するようにした。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,9 @@ class UsersController < ApplicationController
       user.destroy!
       redirect_to root_path, notice: '退会完了しました'
     else
-      raise '認証に失敗しました'
-      write_failure_log('authentication failed.')
+      write_failure_log("authentication failed. #{user.class} id:#{user.id}")
+      render file: 'public/400.html', layout: false, content_type: 'text/html',
+        status: :unauthorized
     end
   end
 end
