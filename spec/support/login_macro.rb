3bd07bd7edeb391f161036869be0875d339d0e75
shin-m <fen.mex.cyc@gmail.com>
2017-04-09 21:43:14 +0900

    [Modify]login_as_twitter_userメソッドの引数をモデルのインスタンスに変更
    E2EテストでログインするTwitterユーザと呼び出し元で保持しているユーザが
    同じユーザとなるようにuidを指定できる必要があった。
    キーワード引数にuidを追加する方法よりも、ファクトリ等から生成したモデルの
    インスタンスをそのまま渡せる方が呼び出し元で保持するモデルのインスタンスと
    ログインするユーザが同一であることがわかりやすいと考えモデルのインスタンスを
    引数にした。

--- spec/support/login_macro.rb
+++ spec/support/login_macro.rb
@@ -5,14 +5,15 @@ module LoginMacro
     find_link('ログアウト')
   end
 
-  def login_as_twitter_user(name: 'name', nickname: 'nickname')
+  def login_as_twitter_user(twitter_user = nil)
+    twitter_user = FactoryGirl.build(:twitter_user, :with_user) unless twitter_user
     OmniAuth.config.mock_auth[:twitter] = OmniAuth::AuthHash.new({
       provider: 'twitter',
-      uid: '123545',
+      uid: twitter_user.uid,
       info: {
-        nickname: nickname,
-        name: name,
-        image: '/assets/guest_user.png'
+        nickname: twitter_user.nickname,
+        name: twitter_user.user.username,
+        image: twitter_user.user.image_url
       }
     }) unless OmniAuth.config.mock_auth[:twitter]
     find_link('Twitterでログイン').click
