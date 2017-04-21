46a81f97ef3f4d96240e524d6a13ed9cfad8dc08
shin-m <fen.mex.cyc@gmail.com>
2017-04-21 11:34:36 +0900

    [Clean]AbcSizeの超過を修正
    不要なunlessを削除。引数のデフォルト値がnilだったが、すぐに代入しており
    冗長だったためデフォルト値を変更。

--- spec/support/login_macro.rb
+++ spec/support/login_macro.rb
@@ -5,20 +5,17 @@ module LoginMacro
     find_link('ログアウト')
   end
 
-  def login_as_twitter_user(twitter_user = nil)
-    twitter_user = FactoryGirl.build(:twitter_user, :with_user) unless twitter_user
-    unless OmniAuth.config.mock_auth[:twitter]
-      OmniAuth.config.mock_auth[:twitter] =
-        OmniAuth::AuthHash.new(
-          provider: 'twitter',
-          uid: twitter_user.uid,
-          info: {
-            nickname: twitter_user.nickname,
-            name: twitter_user.user.username,
-            image: twitter_user.user.image_url
-          }
-        )
-    end
+  def login_as_twitter_user(twitter_user = FactoryGirl.build(:twitter_user, :with_user))
+    OmniAuth.config.mock_auth[:twitter] ||=
+      OmniAuth::AuthHash.new(
+        provider: 'twitter',
+        uid: twitter_user.uid,
+        info: {
+          nickname: twitter_user.nickname,
+          name: twitter_user.user.username,
+          image: twitter_user.user.image_url
+        }
+      )
     find_link('Twitterでログイン').click
     find_link('ログアウト')
   end
