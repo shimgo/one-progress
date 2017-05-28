914cae6aafff857807ee5598b25d9c617f80d908
shimgo <fen.mex.cyc@gmail.com>
2017-05-28 21:56:30 +0900

    [Fix]編集ボタンのモーダルダイアログがすぐに閉じてしまうバグを修正
    AwesomeFont導入時にBootstrapを読み込む処理を追加したが、Bootstrapは既に
    読み込まれており、Toggleが2回呼び出されることでダイアログがすぐ閉じられて
    しまっていた。
    現在AwesomeFontはTwitterアイコンの表示のためだけに使用しており、問題なく
    動作することから、AwesomeFont導入時に追加したbootstrap-sprocketsを削除した。

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
-//= require bootstrap-sprockets
 //= require turbolinks
 //= require jquery.infinitescroll
 //= require masonry.pkgd.min.js
