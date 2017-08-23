35f06b53b83f6a364356677e522520b36de58556
shimgo <fen.mex.cyc@gmail.com>
2017-08-23 23:31:49 +0900

    [Fix]アプリケーションコンテナ起動時にDBのデータが消えるバグを修正
    rake db:setupはdb:scheme:loadを含んでおり、それによりテーブルが再作成されるため
    コンテナ起動のたびにデータが消えてしまっていたのでdb:createとdb:migrateを個別に
    実行するように変更。

--- provisioning/playbook/roles/containers/files/containers/rails/entrypoint.sh
+++ provisioning/playbook/roles/containers/files/containers/rails/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
-bundle exec rake db:setup
+bundle exec rake db:create
+bundle exec rake db:migrate
 bundle exec rake assets:precompile
 bundle exec rake error_page:gen
 export SECRET_KEY_BASE=`bundle exec rake secret -s`
