e43648b8a6ef929e198f0bfd9410e3cba398d21a
shimgo <fen.mex.cyc@gmail.com>
2017-09-09 19:55:40 +0900

    [Add]テスト実行前にエラーページ生成タスクを実行
    エラーページの生成はタスクに分離しており、このタスクを一度も実行していない場合、
    エラーページを確認するテストが失敗してしまう。タスクの実行を忘れないようにテスト
    実行前に必ず実行するようにする。

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,7 @@ abort('The Rails environment is running in production mode!') if Rails.env.produ
 require 'spec_helper'
 require 'rspec/rails'
 require 'capybara/poltergeist'
+require 'rake'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -61,6 +62,8 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
 
   config.before(:suite) do
+    generate_error_page
+
     if config.use_transactional_fixtures?
       raise(<<-MSG)
         Delete line `config.use_transactional_fixtures = true` from rails_helper.rb
