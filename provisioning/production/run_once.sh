36abb5d3768d14423c8e2dabeb488c72450ef47a
shimgo <fen.mex.cyc@gmail.com>
2017-09-13 22:50:59 +0900

    [Add]プロダクション環境のプロビジョニング設定を追加
    VPSに対してプロビジョニングを実行するVagrantfileやPlaybookの設定を追加。
    プロビジョニングを実行するディレクトリが、プロジェクトルート(ステージング環境)と
    provisioning/production(プロダクション環境)の2つになるため、ソースコードの配置先
    をvagrantの自動同期に依存しないように変更した。それに伴いソースコードの所有者が
    rootにならないようにbecome: noを指定している。
    
    プロビジョニングを実行する前にVPSは以下のセットアップを完了しておく必要がある。
     * vagrantユーザを作成
     * vagrantユーザでパスなしsudoを実行可能
     * 公開鍵認証でvagrantユーザでssh可能
     * SELinuxが有効
    
    プロダクション環境のプロビジョニングをするにはprovisioning/productionに移動して
    vagrant upを実行後、vagrant provisionを実行する。
    provisioning/production/configにはプロダクション環境用のVagrantfileで使用する
    設定ファイルを配置する。現時点では公開したくない設定しか存在しないが、config
    ディレクトリは登録したいので.gitkeepを追加している。
    
    初回のプロビジョニングが完了するとVPSのSSHポートが変更される。変更されたSSH
    ポートを意識することなく操作できるように、初回のプロビジョニングが完了すると
    playbook/host_vars/one-progress.ymlにAnsible用のSSHポート設定をするファイルを
    作成する。vagrantはこのファイルの存在を確認してVagrantfile内でSSHポートを
    切り替えている。
    この実装はone-progress.ymlで設定する情報がSSHポートのみである、という前提に依存
    している。現時点では設定したい他の情報が思いつかないのでこのままとするが、他にも
    情報が必要になった場合には、そもそもSSHポート変更に対応するかどうかも含め
    考え直す必要があるかもしれない。

---
+++ provisioning/production/run_once.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+echo "policycoreutils-pythonをインストール"
+yum install -y policycoreutils-python
+
+echo "SELinuxをpermissiveに設定"
+setenforce 0
+
+echo "firewalldを起動"
+systemctl start firewalld.service
+
+echo "firewalldからsshサービスを削除"
+firewall-cmd --permanent --zone=public --remove-service=ssh
+
+echo "firewalldに$1/tcpポートを追加"
+firewall-cmd --permanent --zone=public --add-port=$1/tcp
+
+echo "sshdのポートを変更"
+sed -i "s/^#.*Port 22/Port $1/g" /etc/ssh/sshd_config 
+
+echo "sshdを再起動"
+systemctl restart sshd.service
+
+echo 'SELinuxで新しいsshポートを許可'
+semanage port -a -t ssh_port_t -p tcp $1
+
+echo "SELinuxをenforceに設定"
+setenforce 1
